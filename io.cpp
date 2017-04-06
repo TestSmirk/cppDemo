2e47270c5e4333552af162dd330e8c006622d07f
TestSmirk <d.kc@qq.com>
2017-04-06 19:34:15 +0800

    some cpp file

---
+++ io.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+
+	cout<<"please input int: "<<endl;
+	int x =0;
+	cin>>x;
+	cout<<oct<<x<<endl;
+	cout<<dec<<x<<endl;
+	cout<<hex<<x<<endl;
+	cout<<"pleaase input bool"<<endl;
+	bool y = false;
+	cin>>y;
+	cout<<boolalpha<<y<<endl;
+	return 0;
+}
---
+++ namespace.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	
+	using namespace std;
+	string str;
+
+	int age;
+	cin>>str>>age;
+	cout<<str<<"已经成立"<<age<<"年了. "<<endl;
+	return 0;
+}
